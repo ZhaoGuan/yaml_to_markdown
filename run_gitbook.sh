897504e63165ce3b7c9f377219c6e5da298544c3
ZhaoGuan <guanzhaogz@gmail.com>
2020-09-07 14:35:56 +0800

    run_gitbook.sh中修改记录用的接口host

--- run_gitbook.sh
+++ run_gitbook.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+host=$1
+# 指定记录服务的host
+sed -i 's/0.0.0.0:8000/'"${host}"'/g' ./module/recording.html
 # 关闭之前的gitbook服务
+# shellcheck disable=SC2009
 ps -ax | grep gitbook | grep -v grep | awk '{ print $1 }' | xargs kill -2
 # 创建md_cases文件夹
 rm -rf ./md_cases
@@ -12,6 +16,7 @@ cp ./module/recording.html ./md_cases
 cp ./module/recording.html ./md_cases
 cp ./module/README.md ./md_cases
 cp ./module/book.json ./md_cases
+# shellcheck disable=SC2164
 cd md_cases
 gitbook install
 #nohup gitbook serve &
